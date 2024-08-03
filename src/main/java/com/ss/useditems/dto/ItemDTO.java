8b38642577657150bbd21e224c7329182f17ae58
bus94 <lbs927@naver.com>
2024-08-03 15:24:29 +0900

    ItemDTO

--- src/main/java/com/ss/useditems/dto/ItemDTO.java
+++ src/main/java/com/ss/useditems/dto/ItemDTO.java
@@ -13,16 +13,16 @@ public class ItemDTO {
 	
 	private int item_index;
 	private int item_seller;
-	private String item_title	;
-	private String item_content	;
-    private String item_image	;
-	private String item_category	;
+	private String item_title;
+	private String item_content;
+    private String item_image;
+	private String item_category;
 	private String item_place;
-	private String item_status	;
-	private Date item_enrollDate	;
+	private String item_status;
+	private Date item_enrollDate;
 	private int item_click;
 	private int item_interest;
 	private Date item_offDate;
-	private int item_buyer	;
+	private int item_buyer;
 
 }
