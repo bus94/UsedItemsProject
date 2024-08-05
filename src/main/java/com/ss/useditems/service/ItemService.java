59d5aad2364f5dd692574bfd2b49daf2171ec528
bus94 <lbs927@naver.com>
2024-08-05 14:22:13 +0900

    수정

--- src/main/java/com/ss/useditems/service/ItemService.java
+++ src/main/java/com/ss/useditems/service/ItemService.java
@@ -1,10 +1,7 @@
 package com.ss.useditems.service;
 
-<<<<<<< HEAD
 import java.util.HashMap;
-=======
 import java.util.ArrayList;
->>>>>>> 3b57cebb56ba316ecd797a822427341ee94b1c60
 import java.util.List;
 import java.util.Map;
 
@@ -20,11 +17,17 @@ public class ItemService {
 
 	@Autowired
 	private ItemMapper mapper;
-<<<<<<< HEAD
 
-	public List<ItemDTO> searchItems(String searchValue) {
-		System.out.println(mapper.searchItems(searchValue));
-		return mapper.searchItems(searchValue);
+	public PageInfo searchItems(int currentPage, String searchValue) {
+		ArrayList<ItemDTO> unpaged_list = mapper.searchItems(searchValue);
+
+		PageInfo pageinfo = new PageInfo(currentPage, 5, unpaged_list.size(), 10);
+
+		List<ItemDTO> paged_list = unpaged_list.subList(pageinfo.getFromIndex(), pageinfo.getTillIndex());
+
+		pageinfo.setDtoContainer2(paged_list);
+
+		return pageinfo;
 	}
 
 	public List<ItemDTO> interestItem(int accIndex) {
@@ -38,31 +41,6 @@ public class ItemService {
 		params.put("accIndex", accIndex);
 		params.put("itemId", itemId);
 		return mapper.deleteInterestItem(params) > 0;
-
 	}
 
-=======
-	
-	  public PageInfo searchItems(int currentPage,String searchValue) { 
-		  ArrayList<ItemDTO> unpaged_list = mapper.searchItems(searchValue);
-		  
-		  PageInfo pageinfo = new PageInfo(currentPage, 5, unpaged_list.size(), 10);
-		  
-		  List<ItemDTO> paged_list = unpaged_list.subList(pageinfo.getFromIndex(), pageinfo.getTillIndex());
-		  
-		  pageinfo.setDtoContainer2(paged_list);
-			
-		  return pageinfo;
-	  }
-	 
-	  public List<ItemDTO> interestItem(int accIndex){
-		  System.out.println(mapper.interest(accIndex));
-		  return mapper.interest(accIndex);
-	  }
-	  
-	    public void deleteInterestItem(int accIndex, int itemId) {
-	        mapper.deleteInterestItem(accIndex, itemId);
-	    }
-	  
->>>>>>> 3b57cebb56ba316ecd797a822427341ee94b1c60
 }
