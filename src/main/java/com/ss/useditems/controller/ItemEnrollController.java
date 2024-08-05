480698c70d9e71c6ca73a29d07f5146e9e1be124
bus94 <lbs927@naver.com>
2024-08-05 17:24:46 +0900

    file 업로드 수정 중

--- src/main/java/com/ss/useditems/controller/ItemEnrollController.java
+++ src/main/java/com/ss/useditems/controller/ItemEnrollController.java
@@ -1,11 +1,18 @@
 package com.ss.useditems.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ss.useditems.dto.ItemDTO;
 import com.ss.useditems.dto.MemberDTO;
@@ -59,5 +66,45 @@ public class ItemEnrollController {
 			model.addAttribute("location", "/item/itemEnroll.do");
 		}
 		return "common/msg";
+	}
+	
+	@PostMapping("/uploadOK")
+	public String upload(@RequestParam("file") MultipartFile file, Model model) {
+		// 업로드한 파일명 가져오기
+		String fileRealName = file.getOriginalFilename();
+		if(fileRealName == null || fileRealName.isEmpty()) {
+			System.out.println("파일 존재x");
+			model.addAttribute("msg", "파일이 존재하지 않거나 업로드에 실패하였습니다.");
+			model.addAttribute("location", "/item/itemEnroll.do");
+			return "common/msg";
+		}
+		
+		 // 파일 사이즈 (용량 클 수 있으므로 int가 아닌 long) 
+		long size = file.getSize();
+		System.out.println("파일명: " + fileRealName); 
+		System.out.println("파일 크기: " + size);
+		 
+		// 서버에 저장할 파일명으로 확장자명 지정
+		String fileExtension = fileRealName.substring(fileRealName.lastIndexOf("."), fileRealName.length());
+		String uploadFolder = "C:\\test\\upload";
+		
+		// 디렉토리 존재 여부 확인 후 없으면 생성
+		File directory = new File(uploadFolder);
+		if(!directory.exists()) {
+			directory.mkdirs();
+		}
+
+		// UUID 적용
+		String uniqueName = UUID.randomUUID().toString();
+		File saveFile = new File(uploadFolder + "\\" + uniqueName + fileExtension);
+		try {
+			// 실제 파일 저장 메서드
+			file.transferTo(saveFile);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "item/itemEnroll";
 	}
 }
