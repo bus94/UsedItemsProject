ba971e88c9bb0d8c1c1d0c9bbed1c9ba1712dad9
bus94 <lbs927@naver.com>
2024-08-09 11:11:45 +0900

    itemInfoDTO 적용 중

--- src/main/java/com/ss/useditems/controller/BlacklistController.java
+++ src/main/java/com/ss/useditems/controller/BlacklistController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
