37babfe43569ba4a36e79b5c3284f2dea4fc6dcc
ocy9911 <dhcksdid99@gmail.com>
2024-08-14 23:51:43 +0900

    ocy

--- src/main/java/com/ss/useditems/controller/ItemViewController.java
+++ src/main/java/com/ss/useditems/controller/ItemViewController.java
@@ -35,8 +35,14 @@ public class ItemViewController {
 		System.out.println("itemView 페이지");
 		System.out.println("item_index controlloer: " + loginMember);
 
-	
+		boolean isInterested=false;
+		if(loginMember!=null){
+			isInterested=service.isInterest(acc_item_index(loginMember.getAcc_index(),item_index));
 
+			acc_item_index(loginMember.getAcc_index(),item_index);
+		}
+
+		
 		// 조회수 증가 로직
 		String cookieName = "itemView_" + item_index;
 		Cookie[] cookies = request.getCookies();
