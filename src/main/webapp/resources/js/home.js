a0cca2ba095d10c360d88172c3c640a3ea85136b
moong <cdn02147@naver.com>
2024-08-18 18:03:58 +0900

    items

--- src/main/webapp/resources/js/home.js
+++ src/main/webapp/resources/js/home.js
@@ -43,7 +43,7 @@ window.addEventListener('scroll', function() {
 
 /* 메인 뉴아이템*/
 
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const prevButton = document.querySelector('.new_arrow.prev');
     const nextButton = document.querySelector('.new_arrow.next');
     const container = document.querySelector('.newItem_container2');
@@ -108,7 +108,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
     
 
+});*/
+
+$('.newItem_container2').slick({
+  dots: false,
+  infinite: true,
+  arrows:true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
+
+
+
 
 
 
