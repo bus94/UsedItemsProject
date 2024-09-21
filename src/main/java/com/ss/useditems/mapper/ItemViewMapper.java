e4b97a623df06a2fb4980f9f81a7f096894b066b
moong <cdn02147@naver.com>
2024-09-21 20:10:29 +0900

    itemView 수정

--- src/main/java/com/ss/useditems/mapper/ItemViewMapper.java
+++ src/main/java/com/ss/useditems/mapper/ItemViewMapper.java
@@ -13,35 +13,48 @@ import com.ss.useditems.dto.ReplyDTO;
 @Mapper
 public interface ItemViewMapper {
 
+	// 물품 정보 가져오기
 	ItemInfoDTO selectByItemIndex(int item_index);
 
+	// 물품 셀러 정보 불러오기
 	MemberDTO selectByIndex(int item_index);
 
+	// 댓글 등록
 	int insertReply(ReplyDTO dto);
 
+	// 댓글 불러오기
 	List<ReplyDTO> selectReplyByItemIndex(int item_index);
 
+	// 조회수 증가
 	void incrementViews(int item_index);
 
+//	int interestnum(int item_index);
 
-	int interestnum(int item_index);
-
+	// 댓글삭제
 	int deleteReply(Map<String, Integer> hmap);
 
+	// 채팅방이 있는지 확인
 	int checkChatRoom(Map<String, Integer> hmap);
 	
+	// 물품 onsale로 변경
 	int updateItemOnsale(Map<String, Integer> hmap);
 	
+	// 댓글 수정
 	int updateReply(ReplyDTO dto);
 
+	// 셀러의 다른 물품 불러오기
 	List<ItemInfoDTO> selectByItemSeller(HashMap<String, Integer> map);
 
+	// 관심수 증가
 	void addInterest(Map<String,Integer> map);
 
+	// 관심등록확인
 	int checkInterest(Map<String, Integer> map);
 
+	// 관심등록 해제
 	void removeInterest(Map<String, Integer> map);
 
+	// 물품 삭제
 	int deleteItem(int item_index);
 
 
